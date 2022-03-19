c168e8d568a46837e02498901b5640777f0dd502
Priyanshu_M <priyanshumehra95@gmail.com>
2022-03-20 01:23:47 +0530

    changes in backend

--- chatapp-backend/public/js/friends.js
+++ chatapp-backend/public/js/friends.js
@@ -3,20 +3,20 @@
 var url_string = window.location;
 var url = new URL(url_string);
 var names = url.searchParams.get("name");
+var list=document.getElementById("input");
+var box=document.getElementsByClassName("myfriends")
 var email = url.searchParams.get("email");
 console.log(names,email)
-const u=document.querySelector(".username")
-const submit=document.querySelector(".submit")
 
-submit.addEventListener("click",()=>{
-    u.value=names
-})
-const btn=document.querySelector(".btn")
+
+// const btn=document.querySelector(".btn")
 async function friends(){
     const friends=await fetch("/friendReq")
     const data=await friends.json()
     
     for(let i=0;i<data.length;i++){
+       
+        
         if(data[i].email==email){
         console.log(data[i].friends)
     }
@@ -25,18 +25,31 @@ async function friends(){
     }
 }
 }
-    btn.addEventListener("click",()=>{
-        friends()
-    })
 
 async function getFriends(){
     const friends=await fetch("/friendReq")
     const data=await friends.json()
-    console.log(data[0])
+    console.log(data)
     for(let i=0;i<data.length;i++){
         let div=document.createElement("div")
+        let option=document.createElement("option")
+        console.log(data[i].name)
+        div.classList.add("element")
         div.innerHTML=data[i].name
+        option.innerHTML=data[i].name
+        list.appendChild(option)
+       
         document.body.appendChild(div)
     }
 }
 getFriends()
+
+async function myFunction()
+{
+    document.querySelector('.username').value=names;
+}
+
+myFunction();
+const home=document.querySelector(".home")
+home.setAttribute("href",`//localhost:3000/home.html?name=${names}&email=${email}`)
+    
